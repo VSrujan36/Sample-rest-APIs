aee585b6ff8848ef4a0d879180ea0f62c6d63d13
srujan <vsrujan956@gmail.com>
2024-01-08 18:58:33 -0600

    Added new calculator controller class

---
+++ src/main/java/com/example/amex/controller/Calculator.java
@@ -0,0 +1,17 @@
+package com.example.amex.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/calculator")
+public class Calculator {
+    @GetMapping("/add/{number1}/{number2}")
+    public String addition(@PathVariable Integer number1, @PathVariable Integer number2) {
+        int i = number1 + number2;
+        String sss = String.valueOf(i);
+        return sss;
+    }
+}
