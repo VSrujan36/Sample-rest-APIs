0a180ea0b33d259fb10a6b15c2cf8ab0d989afdd
srujan <vsrujan956@gmail.com>
2024-01-08 18:47:17 -0600

    moving files to local repo

---
+++ src/main/java/com/example/amex/RestTestCodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.amex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestTestCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestTestCodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/amex/RestTestCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.amex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestTestCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
